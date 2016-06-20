a049f3e82823d8693d458233c6a72d2d7a719a98
zocarter <missionbit@missionbits-MacBook-Pro-10.local>
2016-06-20 12:19:11 -0700

    this is the updated version
    updated version

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-var game = new 
-Phaser.Game(1500,1000,Phaser.AUTO); 
+var game = new Phaser.Game(1500, 1000, Phaser.AUTO); 
 
-game.state.add("state0", demo.state0); 
-game.state.start("state0"); 
+game.state.add('state0', demo.state0); 
+game.state.start('state0'); 
--- state0.js
+++ state0.js
@@ -1,38 +1,63 @@
+    var demo = {}; 
+    var speed = 6;
+    var adam;
 
-var demo = {}; 
 
-demo.state0 = function(){};
+    demo.state0 = function(){};
 
-demo.state0.prototype = {
-   
-    preload: function(){ 
-     
-        game.load.image('trees','assets/background/treeBG.png'); 
-        
-        game.load.spritesheet('adam',
-                              'assets/spritesheets/adamSpritesheet.png', 240, 370);
-    },
-    
-    create: function(){
-        
-         game.physics.startSystem(Phaser.Physics.ARCADE);
-        
-        var trees = game.add.sprite(100, 50, 'trees');
-   
-        var adam = game.add.sprite(200, 500, 'adam');
-        
-        game.physics.enable(adam);
-        
-        adam.scale.setTo(1,1);
-        
-        
-},
-    update: function(){
-        
-        if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
-         
-            adam.x = adam.x + 3;
-    } 
+    demo.state0.prototype = {
+       
+        preload: function(){ 
+         game.load.image('trees','assets/background/treeBG.png'); 
+         game.load.spritesheet('adam','assets/spritesheets/adamSpritesheet.png', 240, 370);
+        },
 
-},
-};
+        create: function(){
+              game.world.setBounds(0, 0, 2183, 1000);
+            //(startx,y,endx,y)
+            game.physics.startSystem(Phaser.Physics.ARCADE); 
+        
+
+           var trees = game.add.sprite(0, 0, 'trees');
+            adam = game.add.sprite(0, 450, 'adam');  
+           
+            game.physics.enable(adam);
+            
+            adam.body.collideWorldBounds = true;
+            
+            adam.scale.setTo(1,1);
+            
+            adam.animations.add("walk",[0,1,2,3,4]);
+        
+        },
+
+
+        update: function(){
+
+            if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+                adam.animations.play("walk");
+              adam.x = adam.x + 6; 
+                adam.scale.setTo(1,1);
+                
+            }
+
+            if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+                adam.animations.play("walk");
+                    adam.x = adam.x - 6; 
+                adam.scale.setTo(-1,1);
+            }
+
+            if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
+              if(adam.y < 200 ){
+                  adam.y = 200;
+              }
+                adam.animations.play("walk");
+                adam.y =  adam.y - 6;}
+
+            if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
+                adam.animations.play("walk");
+                adam.y = adam.y + 6; }
+           
+        }
+
+    };
